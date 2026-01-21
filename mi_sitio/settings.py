a455b017d2fcd072708e6b8ad46882894fd499d6
Edwin <edwin2017ca@gmail.com>
2026-01-20 23:27:04 -0500

    Fix: tablas de base de datos y despliegue

--- mi_sitio/settings.py
+++ mi_sitio/settings.py
@@ -137,3 +137,6 @@ if not DEBUG:
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
